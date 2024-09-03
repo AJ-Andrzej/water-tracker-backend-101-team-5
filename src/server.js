3bd03c075b8aa0df024b8780c1ce27b17cba3a6b
AJ-Andrzej <aayaschenko@gmail.com>
2024-09-03 20:47:51 +0200

    update cors

--- src/server.js
+++ src/server.js
@@ -10,11 +10,16 @@ import { env } from './utils/env.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
+const corsOptions = {
+  origin: 'https://water-tracker-101-team-5.vercel.app',
+  optionsSuccessStatus: 200,
+};
+
 const PORT = Number(env('PORT', '3000'));
 export function setupServer() {
   const app = express();
 
-  app.use(cors());
+  app.use(cors(corsOptions));
   app.use('/uploads', express.static(UPLOAD_DIR));
   app.use('/api-docs', swaggerDocs());
   app.use(cookieParser());
